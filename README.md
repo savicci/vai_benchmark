626dd147a0ece0fdd46465bad3dfbf813773a50a
savicci <koziol.grzesiek@interia.pl>
2022-04-27 17:56:26 +0200

    example for pruner

--- README.md
+++ README.md
@@ -12,3 +12,13 @@
 * Speech-to-text - RNN-T with LibriSpeech dataset
 * NLP - BERT with SQuAD v1.1 dataset
 * Recommendation - DLRM with Criteo 1Tb dataset 
+
+### installing env for tensorflow2
+* conda create -n tf_gpu_env python=3.8 
+* conda activate tf_gpu_env
+* conda install tensorflow-gpu -c anaconda
+* conda install cudnn -c conda-forge 
+* conda install cudatoolkit -c anaconda
+* pip install --upgrade tensorflow
+* pip install jupyter
+* pip install pillow
---
+++ pruner_flow.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tf_nndct.optimization import IterativePruningRunner
+
+input_shape = (28, 28, 1)
+num_classes = 10
+
+# load dataset
+(ds_train, ds_test) = tfds.load('fashion_mnist', split=['train', 'test'], as_supervised=True, shuffle_files=True)
+
+# create model
+base_model = tf.keras.Sequential([
+    tf.keras.Input(shape=input_shape),
+    tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Flatten(), tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(num_classes, activation="softmax"),
+])
+
+input_spec = tf.TensorSpec((1, *input_shape), tf.float32)
+runner = IterativePruningRunner(base_model, input_spec)
+
+x_test = []
+y_test = []
+
+
+def add_normalized_values(img, label):
+    norm_img = tf.cast(img, dtype=tf.float32) / 255.0
+    x_test.append(norm_img)
+    y_test.append(label)
+    return norm_img, label
+
+
+# map data
+ds_test.map(add_normalized_values, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+ds_test = ds_test.batch(32)
+ds_train = ds_train.batch(32)
+
+# train
+base_model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+base_model.fit(ds_train, epochs=15)
+
+
+def evaluate(model):
+    model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    score = model.evaluate(x_test, y_test, verbose=0)
+    return score[1]
+
+
+# evaluation
+runner.ana(evaluate)
+
+# prune
+sparse_model = runner.prune(ratio=0.2)
+
+sparse_model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+sparse_model.fit(ds_train, batch_size=32, epochs=15, validation_split=0.1)
+sparse_model.save_weights("model_sparse_0.2", save_format="tf")
