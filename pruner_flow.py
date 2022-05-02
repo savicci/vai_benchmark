98870f529e2f984d6ca87b3830cb296a72d0df5d
savicci <koziol.grzesiek@interia.pl>
2022-05-02 12:32:36 +0200

    fixes

--- pruner_flow.py
+++ pruner_flow.py
@@ -2,11 +2,64 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 from tf_nndct.optimization import IterativePruningRunner
 
+
+def add_normalized_values(img, label):
+    '''Normalizes images'''
+    norm_img = tf.cast(img, dtype=tf.float32) / 255.0
+    return norm_img, label
+
+
+def evaluate(model):
+    '''Function used by Pruner to evaluate pruning performance'''
+    model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    score = model.evaluate(ds_test, verbose=0)
+    return score[1]
+
+
+def pruning_loop(model):
+    ratio = 0.5
+    iteration = 0
+
+    input_spec = tf.TensorSpec((1, *input_shape), tf.float32)
+    curr_accuracy = evaluate(model)
+
+    while curr_accuracy >= 0.85:
+        print("Iteration {}".format(iteration))
+
+        # setup
+        filename = "pruned/model_sparse_{}".format(iteration)
+        pruning_runner = IterativePruningRunner(model, input_spec)
+        pruning_runner.ana(evaluate)
+
+        # run pruning
+        sparse_model = pruning_runner.prune(ratio=ratio)
+        sparse_model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+        sparse_model.fit(ds_train, epochs=15)
+        sparse_model.save_weights(filename, save_format="tf")
+
+        # post pruning setup for next iteration
+        iteration += 1
+        curr_accuracy = sparse_model.evaluate(ds_test)[1]
+        model.load_weights(filename)
+
+        # decrease pruning ratio
+        ratio *= 0.8
+
+    return model
+
+
 input_shape = (28, 28, 1)
 num_classes = 10
 
 # load dataset
 (ds_train, ds_test) = tfds.load('fashion_mnist', split=['train', 'test'], as_supervised=True, shuffle_files=True)
+
+# map data
+ds_test = ds_test.batch(32)
+ds_train = ds_train.batch(32)
+
+ds_train = ds_train.map(add_normalized_values, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_test = ds_test.map(add_normalized_values, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
 # create model
 base_model = tf.keras.Sequential([
@@ -19,38 +72,17 @@ base_model = tf.keras.Sequential([
     tf.keras.layers.Dense(num_classes, activation="softmax"),
 ])
 
-input_spec = tf.TensorSpec((1, *input_shape), tf.float32)
-runner = IterativePruningRunner(base_model, input_spec)
-
-ds_test = ds_test.batch(32)
-ds_train = ds_train.batch(32)
-
-def add_normalized_values(img, label):
-    norm_img = tf.cast(img, dtype=tf.float32) / 255.0
-    return norm_img, label
-
-
-# map data
-ds_test = ds_test.map(add_normalized_values, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
-
 # train
 base_model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 base_model.fit(ds_train, epochs=15)
+base_model.evaluate(ds_test)
 
+base_model.summary()
 
-def evaluate(model):
-    model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-    score = model.evaluate(ds_test, verbose=0)
-    return score[1]
+pruned_model = pruning_loop(base_model)
 
+spec = tf.TensorSpec((1, *input_shape), tf.float32)
+runner = IterativePruningRunner(pruned_model, input_spec)
+pruned_slim_model = runner.get_slim_model()
 
-# evaluation
-runner.ana(evaluate)
-
-# prune
-sparse_model = runner.prune(ratio=0.2)
-
-sparse_model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-sparse_model.fit(ds_train, epochs=15)
-sparse_model.save_weights("model_sparse_0.2", save_format="tf")
+pruned_slim_model.summary()
