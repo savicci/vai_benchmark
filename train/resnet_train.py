db60717e0141d30cef3c84f5daba320e3dcee4c0
savicci <koziol.grzesiek@interia.pl>
2022-05-06 19:49:04 +0200

    checkpoint every 5 epochs

--- train/resnet_train.py
+++ train/resnet_train.py
@@ -5,6 +5,7 @@ physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 tf.config.experimental.set_memory_growth(physical_devices[1], True)
 
+
 def add_normalized_values(img, label):
     """Normalizes images"""
     norm_img = tf.cast(img, dtype=tf.float32) / 255.0
@@ -12,13 +13,14 @@ def add_normalized_values(img, label):
 
 
 # dataset preparation
-(ds_train, ds_validation) = tfds.load('imagenet2012', shuffle_files=True, as_supervised=True, split=['train', 'validation'])
+(ds_train, ds_validation) = tfds.load('imagenet2012', shuffle_files=True, as_supervised=True,
+                                      split=['train', 'validation'])
 
 ds_train = ds_train.map(add_normalized_values, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 ds_validation = ds_validation.map(add_normalized_values, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-ds_train = ds_train.batch(192)
-ds_validation = ds_validation.batch(192)
+ds_train = ds_train.batch(100)
+ds_validation = ds_validation.batch(100)
 
 # load model
 float_model = tf.keras.models.load_model('../models/resnet_50.h5')
@@ -29,7 +31,9 @@ float_model.compile(
     metrics='accuracy'
 )
 
-float_model.fit(ds_train, epochs=150)
+checkpoint_path = '/workspace/vai_benchmark/data/train/checkpoints/resnet.ckpt'
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=False, verbose=1, period=5)
+
+float_model.fit(ds_train, validation_data=ds_validation, epochs=100, callbacks=[cp_callback])
 
 float_model.save('../models/resnet_50_trained.h5')
-
