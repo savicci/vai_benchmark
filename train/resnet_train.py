cf7b43d617ab2966d8e5d67fa1aa08cad7c6de75
savicci <koziol.grzesiek@interia.pl>
2022-05-06 19:09:19 +0200

    add resnet train

---
+++ train/resnet_train.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+def add_normalized_values(img, label):
+    """Normalizes images"""
+    tf.image.resize(img, [224, 224])
+    norm_img = tf.cast(img, dtype=tf.float32) / 255.0
+    return norm_img, label
+
+
+# dataset preparation
+(ds_train, ds_validation) = tfds.load('imagenet2012', shuffle_files=True, as_supervised=True, split=['train', 'validation'])
+
+ds_train = ds_train.batch(192)
+ds_validation = ds_validation.batch(192)
+
+ds_train = ds_train.map(add_normalized_values, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_validation = ds_validation.map(add_normalized_values, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+
+# load model
+float_model = tf.keras.models.load('../models/resnet_50.h5')
+
+float_model.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics='accuracy'
+)
+
+float_model.evaluate(ds_validation, verbose=2)
+
+
+
