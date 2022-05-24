62d0ea4c37334b5ecbe65b8b2ba37f1d15ce631d
savicci <koziol.grzesiek@interia.pl>
2022-05-24 18:49:25 +0200

    prune trained resnet

--- pruning/resnet_prune.py
+++ pruning/resnet_prune.py
@@ -1,5 +1,14 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from tf_nndct.optimization import IterativePruningRunner
+import argparse
+
+input_shape = (224, 224, 3)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--ratio', default=None, help='Ratio to prune. Float value from 0 to 1', type=float)
+args = parser.parse_args()
+
 
 def add_normalized_values(img, label):
     """Normalizes images"""
@@ -7,8 +16,15 @@ def add_normalized_values(img, label):
     return tf.image.resize(norm_img, [224, 224]), label
 
 
+def evaluate(model):
+    """Function used by Pruner to evaluate pruning performance"""
+    model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    score = model.evaluate(ds_validation, verbose=0)
+    return score[1]
+
+
 # load trained model
-model = tf.keras.models.load_model('../models/resnet_50_trained.h5')
+trained_model = tf.keras.models.load_model('/workspace/vai_benchmark/models/resnet_50_trained.h5')
 
 # dataset preparation
 (ds_train, ds_validation) = tfds.load('imagenet2012', shuffle_files=True, as_supervised=True,
@@ -17,7 +33,20 @@ model = tf.keras.models.load_model('../models/resnet_50_trained.h5')
 ds_train = ds_train.map(add_normalized_values, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 ds_validation = ds_validation.map(add_normalized_values, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
-ds_train = ds_train.batch(100)
-ds_validation = ds_validation.batch(100)
+ds_train = ds_train.batch(64)
+ds_validation = ds_validation.batch(64)
+
+input_spec = tf.TensorSpec((1, *input_shape), tf.float32)
+pruning_runner = IterativePruningRunner(trained_model, input_spec)
+
+pruning_runner.ana(evaluate)
+
+pruned_model = pruning_runner.prune(ratio=args.ratio)
+
+print('Evaluation after pruning')
+pruned_model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+print(pruned_model.evaluate(ds_validation, verbose=0))
+
+
 
 
