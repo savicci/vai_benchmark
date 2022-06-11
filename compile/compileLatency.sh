ddbb780b8e82f2636afe8aade74926e5e52dd4cd
savicci <koziol.grzesiek@interia.pl>
2022-06-11 12:32:14 +0200

    compile

---
+++ compile/compileLatency.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Correct usage: source compileLatency.sh <PATH_TO_MODEL_H5> <COMPILED_BUILD_NAME> <NAME OF NET>"
+    exit
+fi
+
+
+PATH_TO_MODEL=$1
+TARGET=$2
+NETNAME=$3
+
+ARCH=/opt/vitis_ai/compiler/arch/DPUCAHX8L/U280/arch.json
+
+vai_c_tensorflow2 -m $PATH_TO_MODEL -a /PATH/TO/arch.json -o build/compiled_$TARGET -n $NETNAME
---
+++ compile/compileThroughput.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Correct usage: source compileThroughput.sh <PATH_TO_MODEL_H5> <COMPILED_BUILD_NAME> <NAME OF NET>"
+    exit
+fi
+
+PATH_TO_MODEL=$1
+OUTPUTPATH=$2
+NETNAME=$3
+
+ARCH=/opt/vitis_ai/compiler/arch/DPUCAHX8H/U280/arch.json
+
+vai_c_tensorflow2 -m $PATH_TO_MODEL -a /PATH/TO/arch.json -o $OUTPUTPATH -n $NETNAME
