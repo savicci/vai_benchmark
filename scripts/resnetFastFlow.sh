c44a2ac1f8c89d2525375b87122e764232463cd4
savicci <koziol.grzesiek@interia.pl>
2022-06-17 17:46:50 +0200

    adjust batches

--- scripts/resnetFastFlow.sh
+++ scripts/resnetFastFlow.sh
@@ -17,18 +17,18 @@ ARCH=/opt/vitis_ai/compiler/arch/DPUCAHX8H/U280/arch.json
 # use tf2 env
 # train network
 conda activate vitis-ai-tensorflow2
-python ../train/resnet_custom_train.py --batch_size 40 --epochs 10 --workspace $WORKSPACE --prefix $PREFIX
+python ../train/resnet_custom_train.py --batch_size 64 --epochs 10 --workspace $WORKSPACE --prefix $PREFIX
 
 # setup for pruning
 # prune network
 conda activate vitis-ai-optimizer_tensorflow2
 export XILINXD_LICENSE_FILE=/workspace/vai_optimizer.lic
-python ../pruning/resnet_custom_prune.py --batch_size 40 --epochs 2 --workspace $WORKSPACE --prefix $PREFIX --ratio $RATIO
+python ../pruning/resnet_custom_prune.py --batch_size 64 --epochs 2 --workspace $WORKSPACE --prefix $PREFIX --ratio $RATIO
 
 # setup for quantizing
 # quantize network
 conda activate vitis-ai-tensorflow2
-python ../quant/resnet_custom_quantize.py --batch_size 10 --fast_ft_epochs 10 --workspace $WORKSPACE --prefix $PREFIX --calibrations $CALIBRATION_STEPS
+python ../quant/resnet_custom_quantize.py --batch_size 16 --fast_ft_epochs 10 --workspace $WORKSPACE --prefix $PREFIX --calibrations $CALIBRATION_STEPS
 
 # compile network
 vai_c_tensorflow2 -m $WORKSPACE/$PREFIX/quantized/resnet_model.h5 -a $ARCH -o $WORKSPACE/$PREFIX/compiled -n resnet
