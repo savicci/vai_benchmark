b33f7868b37226efcce90a0b684588afc4ef31c3
savicci <koziol.grzesiek@interia.pl>
2022-06-17 17:42:15 +0200

    increase batch size test

--- scripts/resnetFastFlow.sh
+++ scripts/resnetFastFlow.sh
@@ -17,13 +17,13 @@ ARCH=/opt/vitis_ai/compiler/arch/DPUCAHX8H/U280/arch.json
 # use tf2 env
 # train network
 conda activate vitis-ai-tensorflow2
-python ../train/resnet_custom_train.py --batch_size 32 --epochs 10 --workspace $WORKSPACE --prefix $PREFIX
+python ../train/resnet_custom_train.py --batch_size 40 --epochs 10 --workspace $WORKSPACE --prefix $PREFIX
 
 # setup for pruning
 # prune network
 conda activate vitis-ai-optimizer_tensorflow2
 export XILINXD_LICENSE_FILE=/workspace/vai_optimizer.lic
-python ../pruning/resnet_custom_prune.py --batch_size 32 --epochs 2 --workspace $WORKSPACE --prefix $PREFIX --ratio $RATIO
+python ../pruning/resnet_custom_prune.py --batch_size 40 --epochs 2 --workspace $WORKSPACE --prefix $PREFIX --ratio $RATIO
 
 # setup for quantizing
 # quantize network
