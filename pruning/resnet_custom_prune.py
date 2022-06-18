f0d5ff86e4d068512c76a2a1ba10579fb520da47
savicci <koziol.grzesiek@interia.pl>
2022-06-18 15:11:33 +0200

    disable memory growth cause done on training stage

--- pruning/resnet_custom_prune.py
+++ pruning/resnet_custom_prune.py
@@ -101,10 +101,6 @@ if __name__ == '__main__':
     print(' --workspace   : ', args.workspace)
     print(' --prefix   : ', args.prefix)
 
-    physical_devices = tf.config.list_physical_devices('GPU')
-    for device in physical_devices:
-        tf.config.experimental.set_memory_growth(device, True)
-
     # load dataset. Needs to be done earlier for evaluate function
     ds_train, ds_test = load_dataset(args.batch_size)
 
