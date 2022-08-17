30ea81fb80038d597fca5d9e53da640b42b9c63c
savicci <koziol.grzesiek@interia.pl>
2022-08-17 23:18:58 +0200

    set batch size explicite for calib

--- quant/fmnist_resnet_quant.py
+++ quant/fmnist_resnet_quant.py
@@ -25,11 +25,11 @@ def app(batch_size, epochs, path, model_path):
     quantizer = vitis_quantize.VitisQuantizer(model)
 
     # quantize without fine-tuning
-    quantized_model_no_ft = quantizer.quantize_model(calib_dataset=ds_train, calib_batch_size=10, calib_steps=None,
+    quantized_model_no_ft = quantizer.quantize_model(calib_dataset=ds_train, calib_batch_size=5, calib_steps=None,
                                                      include_fast_ft=False)
 
     # quantize with fine-tuning
-    quantized_model_ft = quantizer.quantize_model(calib_dataset=ds_train, calib_batch_size=10, calib_steps=None,
+    quantized_model_ft = quantizer.quantize_model(calib_dataset=ds_train, calib_batch_size=5, calib_steps=None,
                                                   include_fast_ft=True, fast_ft_epochs=10)
 
     # quantization aware training
