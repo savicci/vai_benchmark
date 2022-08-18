51392725c0f568801ea62ab6b3d91e9e4a53e767
savicci <koziol.grzesiek@interia.pl>
2022-08-18 19:18:06 +0200

    increase calibs

--- quant/fmnist_resnet_quant.py
+++ quant/fmnist_resnet_quant.py
@@ -32,11 +32,11 @@ def app(batch_size, epochs, path, model_path):
 
     # quantize without fine-tuning
     print("Start quantizing not ft")
-    quantized_model_no_ft = no_ft_quantizer.quantize_model(calib_dataset=ds_train, calib_steps=10, calib_batch_size=10,
+    quantized_model_no_ft = no_ft_quantizer.quantize_model(calib_dataset=ds_train, calib_steps=50, calib_batch_size=10,
                                                      include_fast_ft=False)
     # quantize with fine-tuning
     print("Start quantizing ft")
-    quantized_model_ft = ft_quantizer.quantize_model(calib_dataset=ds_train, calib_steps=10, calib_batch_size=10,
+    quantized_model_ft = ft_quantizer.quantize_model(calib_dataset=ds_train, calib_steps=50, calib_batch_size=10,
                                                   include_fast_ft=True, fast_ft_epochs=10)
 
     # quantization aware training
