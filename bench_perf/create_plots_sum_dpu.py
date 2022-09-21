df7206cf173daa65ca305cb50ad7ace935709efb
savicci <koziol.grzesiek@interia.pl>
2022-09-21 22:12:33 +0200

    add op type to group

--- bench_perf/create_plots_sum_dpu.py
+++ bench_perf/create_plots_sum_dpu.py
@@ -4,7 +4,7 @@ import csv
 COLS_NUM = 4
 
 
-def create_plot(filename, ylabel, title,  save_filename):
+def create_plot(filename, ylabel, title,  save_filename, op_type):
     rows = []
     with open(filename, 'r') as f:
         reader = csv.reader(f, delimiter=',')
@@ -18,7 +18,14 @@ def create_plot(filename, ylabel, title,  save_filename):
     y = []
     for i in range(0, len(rows), COLS_NUM):
         x.append(float(rows[i]))
-        y.append(float(rows[i + 1]) + float(rows[i + 2]) + float(rows[i + 2]))
+
+        if op_type == 'sum':
+            y.append(float(rows[i + 1]) + float(rows[i + 2]) + float(rows[i + 2]))
+        elif op_type == 'avg':
+            y.append((float(rows[i + 1]) + float(rows[i + 2]) + float(rows[i + 2]))/3)
+        else:
+            print('Unknown op type: {}'.format(op_type))
+            exit(-1)
 
     # plot
     plt.figure()
@@ -33,9 +40,9 @@ def create_plot(filename, ylabel, title,  save_filename):
 
 
 if __name__ == '__main__':
-    create_plot('min_data.csv', 'Time [ms]', 'Minimal inference time', 'min_data')
-    create_plot('avg_data.csv', 'Time [ms]', 'Average inference time', 'avg_data')
-    create_plot('max_data.csv', 'Time [ms]', 'Maximum inference time', 'max_data')
-    create_plot('dpu_perf.csv', 'Workload [GOP/s]', 'DPU Performance', 'dpu_perf')
-    create_plot('mem_io.csv', 'Mem IO [MB]', 'Memory IO', 'mem_io')
-    create_plot('mem_bandwth.csv', 'Mem Bandwidth [MB/s]', 'Memory Bandwidth', 'mem_bandwth')
+    create_plot('min_data.csv', 'Time [ms]', 'Minimal inference time', 'min_data', 'avg')
+    create_plot('avg_data.csv', 'Time [ms]', 'Average inference time', 'avg_data', 'avg')
+    create_plot('max_data.csv', 'Time [ms]', 'Maximum inference time', 'max_data', 'avg')
+    create_plot('dpu_perf.csv', 'Workload [GOP/s]', 'DPU Performance', 'dpu_perf', 'sum')
+    create_plot('mem_io.csv', 'Mem IO [MB]', 'Memory IO', 'mem_io', 'sum')
+    create_plot('mem_bandwth.csv', 'Mem Bandwidth [MB/s]', 'Memory Bandwidth', 'mem_bandwth', 'sum')
